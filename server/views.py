e557abc8a316f65c294b4e961cf15385b56c5292
Uday lal <udaylal014@gmail.com>
2022-09-16 02:01:28 +0530

    updated views.py

--- server/views.py
+++ server/views.py
@@ -1,13 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 views = Blueprint("views", __name__)
 
 
+def getCookie(name: str):
+    return request.cookies.get(name)
+
+def returnTemplate():
+    return render_template("index.html")
+
+
 @views.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    loginToken = getCookie("token")
+    if loginToken is None:
+        return redirect("login")
+    return returnTemplate()
 
 
 @views.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("index.html")
+    loginToken = getCookie("token")
+    if loginToken is not None:
+        return redirect("/")
+    return returnTemplate()
