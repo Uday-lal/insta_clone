b050159795406bd936440d825737f59089bd43bd
Uday lal <udaylal014@gmail.com>
2022-10-01 02:16:08 +0530

    Completed the login form design

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,19 +1,56 @@
 import React from "react";
 import Card from "../../components/card/Card.jsx";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function Login() {
   return (
     <React.Fragment>
       <div className="page">
         <div className="center">
-          <Card>
-            <form>
+          <Card
+            style={{
+              width: "20vw",
+              height: "50vh",
+            }}
+          >
+            <form
+              method="POST"
+              style={{
+                width: "100%",
+              }}
+            >
               <TextField
-                id="outlined-basic"
-                label="Outlined"
+                label="Enter Email"
+                type="email"
+                autoComplete="off"
                 variant="outlined"
+                name="email"
+                style={{
+                  margin: "10px 0px",
+                }}
+                className="w-100"
+                required="true"
               />
+              <TextField
+                label="Enter Password"
+                type="password"
+                autoComplete="off"
+                variant="outlined"
+                name="password"
+                style={{
+                  margin: "10px 0px",
+                }}
+                className="w-100"
+                required="true"
+              />
+              <Button
+                variant="contained"
+                style={{ width: "100%" }}
+                type="submit"
+              >
+                Submit
+              </Button>
             </form>
           </Card>
         </div>
