7dbd2462b948aa0736faca631118182f6873f2ea
Uday lal <udaylal014@gmail.com>
2023-01-03 17:20:01 +0530

    Added post modal

--- frontend/src/pages/home_page/HomePage.jsx
+++ frontend/src/pages/home_page/HomePage.jsx
@@ -6,9 +6,22 @@ import InputBase from "@mui/material/InputBase";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import AddPhotoAlternateRoundedIcon from "@mui/icons-material/AddPhotoAlternateRounded";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import { useState } from "react";
 
 function HomePage(props) {
+  const [openModal, setOpenModal] = useState(false);
   const returnAvatar = (width, height) => {
     if (props.profileImg) {
       return (
@@ -26,8 +39,69 @@ function HomePage(props) {
       );
     }
   };
+  const handleClose = () => {
+    setOpenModal(false);
+  };
   return (
     <React.Fragment>
+      <Dialog
+        fullWidth={true}
+        maxWidth="sm"
+        open={openModal}
+        onClose={handleClose}
+      >
+        <DialogTitle>Post</DialogTitle>
+        <DialogContent>
+          <Box
+            sx={{
+              display: "flex",
+              marginBottom: "10px",
+            }}
+          >
+            {returnAvatar(40, 40)}
+            <Box>
+              <h3 style={{ marginLeft: "10px" }}>{props.userName}</h3>
+            </Box>
+          </Box>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="post"
+            label="What you want to say?"
+            multiline
+            variant="filled"
+            rows={6}
+            fullWidth
+            sx={{ margin: "20px 0px" }}
+          />
+          <FormControl fullWidth>
+            <InputLabel
+              id="visibility-label"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              Visibility
+            </InputLabel>
+            <Select
+              labelId="visibility-label"
+              id="visibility"
+              label="Visibility"
+              // onChange={handleChange}
+            >
+              <MenuItem value={0}>Anyone</MenuItem>
+              <MenuItem value={1}>Only Followers</MenuItem>
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" color="error" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="outlined" onClick={handleClose}>
+            Post
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <Container maxWidth="xl">
         <Container
           maxWidth="xl"
@@ -112,6 +186,8 @@ function HomePage(props) {
                 <InputBase
                   sx={{ ml: 1, flex: 1 }}
                   placeholder="What's on your mind?"
+                  readOnly
+                  onClick={() => setOpenModal(true)}
                   inputProps={{ "aria-label": "what's on your mind" }}
                 />
                 <IconButton aria-label="add photos">
